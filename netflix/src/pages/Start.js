11057a5df86212230af9b10cfb6bfc2e13c93a21
kimyounhyeon <fourkimm@naver.com>
2023-01-18 16:17:35 +0900

    스타트 화면 만드는중 #2

--- netflix/src/pages/Start.js
+++ netflix/src/pages/Start.js
@@ -3,6 +3,7 @@ import Button from 'react-bootstrap/Button';
 import { Row, Col} from "react-bootstrap";
 import SelectLanguage from '../components/SelectLanguage';
 import StartBar from '../components/StartBar';
+import JoinBar from "../components/JoinBar";
 
 const Start = () => {
   return (
@@ -21,6 +22,21 @@ const Start = () => {
             <Col>
             <Button variant="danger" className="start-login-btn">로그인</Button>
             </Col>
+        </Row>
+        <Row className='start-content1'>
+          <h1>영화와 시리즈를</h1>
+        </Row>
+        <Row className='start-content1'>
+          <h1>무제한으로.</h1>
+        </Row>
+        <Row className='start-content1'>
+          <h3>다양한 디바이스에서 시청하세요. 언제든 해지하실 수 있습니다.</h3>
+        </Row>
+        <Row className='start-content1'>
+          <h4>시청할 준비가 되셨나요? 멤버십을 등록하거나 재시작하려면 이메일 주소를 입력하세요.</h4>
+        </Row>
+        <Row className='start-content1'>
+          <JoinBar />
         </Row>
       </div>
       <StartBar />
