3c5e1923991919d4a69dd8c25cd239c8e30f5dfa
kimyounhyeon <fourkimm@naver.com>
2023-01-21 22:58:11 +0900

    select 고침#4

--- netflix/src/components/start/SelectLanguage.js
+++ netflix/src/components/start/SelectLanguage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import {BsGlobe} from 'react-icons/bs'
 
 const SelectLanguage = () => {
   return (
-    <div>
-        <select className='start-language'>
+    <div className='start-language'>
+        <BsGlobe className='select-icon'size={15}/>
+        <select className='select-language'>
               <option>한국어</option>
               <option>English</option>
         </select>
