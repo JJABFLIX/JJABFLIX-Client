215f6c70ede1914f6087ff88504899c665dbcbc7
kimyounhyeon <fourkimm@naver.com>
2023-01-26 00:48:02 +0900

    footer 1구역 완성 #13

--- netflix/src/Routepages.js
+++ netflix/src/Routepages.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Routes, Route } from "react-router-dom";
 import Start from "./pages/Start";
 import Signup from "./pages/Signup";
-import Home from "./pages/Home";
 import Planform from "./pages/Planform";
 import Signup2 from "./pages/Signup2";
 import Signup3 from "./pages/Signup3";
@@ -12,13 +11,12 @@ const Routepages = () => {
   return (
     <div>
         <Routes>
-            <Route path="/start" element={<Start />} />
+            <Route path="/" element={<Start />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/signup2" element={<Signup2 />} />
             <Route path="/signup3" element={<Signup3 />} />
             <Route path="/signup/:id" element={<Planform />} />
             <Route path="/selectpayment" element={<SelectPayment />} />
-            <Route path="/" element={<Home />} />
 
             {/* <Route path="/signup" element={<Signup />}/>
             <Route path="/login" element={<Login />}/>
--- netflix/src/components/start/StartFooter.js
+++ netflix/src/components/start/StartFooter.js
@@ -1,9 +1,41 @@
 import React from 'react'
+import { Row, Col} from "react-bootstrap";
+import SelectLanguage from './SelectLanguage';
+import '../../css/Footer.css'
 
 const StartFooter = () => {
   return (
-    <div>
-      
+    <div className='footer'>
+        <div className='footer-first'>
+        <p>질문이 있으신가요? 문의 전화:  080-001-9587</p>
+        <Row className='footer-list'>
+          <Col>자주 묻는 질문</Col>
+          <Col>고객센터</Col>
+          <Col>계정</Col>
+          <Col>미디어 센터</Col>
+        </Row>
+        <Row className='footer-list'>
+          <Col>투자 정보(IR)</Col>
+          <Col>입사 정보</Col>
+          <Col>넷플릭스 지원 디바이스</Col>
+          <Col>이용 약관</Col>
+        </Row>
+        <Row className='footer-list'>
+          <Col>개인정보 처리방침</Col>
+          <Col>쿠키 설정</Col>
+          <Col>회사 정보</Col>
+          <Col>문의하기</Col>
+        </Row>
+        <Row className='footer-list'>
+          <Col>속도 테스트</Col>
+          <Col>법적 고지</Col>
+          <Col>오직 넷플릭스에서</Col>
+          <Col></Col>
+        </Row>
+        </div>
+        <div>
+          <SelectLanguage />
+        </div>
     </div>
   )
 }
