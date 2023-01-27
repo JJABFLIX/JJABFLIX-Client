cea1fb6c4d2fd644c4c055124d0d18c5d78b0c02
guabaaa <gdandtop950923@gmail.com>
2023-01-27 20:21:30 +0900

    media query 작업 중#14

--- netflix/src/components/start/JoinBar.js
+++ netflix/src/components/start/JoinBar.js
@@ -1,38 +1,54 @@
-import React,{useState} from 'react';
-import { Form } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 const JoinBar = () => {
   const [email, setEmail] = useState("");
   const [emailError, setEmailError] = useState(false);
   const onChangeEmail = (e) => {
-    const emailRegex = /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
-    if (!e.target.value || emailRegex.test(e.target.value)) setEmailError(false);
+    const emailRegex =
+      /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+    if (!e.target.value || emailRegex.test(e.target.value))
+      setEmailError(false);
     else setEmailError(true);
     setEmail(e.target.value);
   };
   const validation = () => {
-    if(!email) setEmailError(true);
+    if (!email) setEmailError(true);
 
-    if(email) return true;
+    if (email) return true;
     else return false;
-  }
+  };
   const onSubmit = (e) => {
-    if(validation()) return;
-  }
+    if (validation()) return;
+  };
 
   return (
     <Form>
-    <div className='join-bar'>
-        <Form.Control type="input" id="email" className='join-input' value={email} onChange={onChangeEmail} autoComplete='off' required />
-        <label for="email"><span id="join-lb">이메일 주소</span></label>
-    
-        <botton className='join-btn' onClick={onSubmit}>시작하기 ></botton>
-    </div>
-    <div>
-    {emailError && <div class="invalid-input">이메일 주소를 입력해 주세요.</div>}
-    </div>
+      <div className="join-bar">
+        <Form.Control
+          type="input"
+          id="email"
+          className="join-input"
+          value={email}
+          onChange={onChangeEmail}
+          autoComplete="off"
+          required
+        />
+        <label for="email">
+          <span id="join-lb">이메일 주소</span>
+        </label>
+
+        <botton className="join-btn" onClick={onSubmit}>
+          시작하기
+        </botton>
+      </div>
+      <div>
+        {emailError && (
+          <div class="invalid-input">이메일 주소를 입력해 주세요.</div>
+        )}
+      </div>
     </Form>
-  )
-}
+  );
+};
 
 export default JoinBar;
